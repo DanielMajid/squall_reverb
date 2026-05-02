a9ce16187bc1d7b837edee504e92ec4759efef1a
Daniel Mirzakhani <191281472+DanielMajid@users.noreply.github.com>
2026-05-02 14:15:10 -0400

    fixed knob A naming issue

--- src/header.c
+++ src/header.c
@@ -11,7 +11,7 @@ const __unit_header unit_header_t unit_header = {
     .num_params = 3,
 
     .params = {
-        {0, 1023, 0, 384, k_unit_param_type_none, 1, 0, 0, {"TIME"}},
+        {0, 1023, 0, 384, k_unit_param_type_none, 1, 0, 0, {"TONE"}},
         {0, 1023, 0, 512, k_unit_param_type_none, 1, 0, 0, {"DPTH"}},
         {-1000, 1000, 0, 0, k_unit_param_type_drywet, 1, 1, 0, {"MIX"}},
         {0, 0, 0, 0, k_unit_param_type_none, 0, 0, 0, {""}},
